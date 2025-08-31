acfb12c19b481e03cc9756131a7ad078ac9801e1
kenro-t <ngkn1425@gmail.com>
2025-08-31 19:02:27 +0900

    テスト

--- lib/my-app-stage.ts
+++ lib/my-app-stage.ts
@@ -1,12 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { S3Stack } from "./s3-stack";
+// import { S3Stack } from "./s3-stack";
 
 // Define the stage
 export class MyAppStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    new S3Stack(this, "S3Stack");
+    // new S3Stack(this, "S3Stack");
   }
 }
